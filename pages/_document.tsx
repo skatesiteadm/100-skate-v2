20db0e76d137b0bc272b690ef8b7241011853085
skatesiteadm <designer@cemporcentoskate.com>
2026-02-24 19:21:26 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,9 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Halyard+Text:ital,wght@0,400;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet" />
       </Head>
-      <body className="bg-white text-black" style={{ fontFamily: "'Halyard Text', sans-serif" }}>
+      <body className="bg-white text-black" style={{ fontFamily: "'Montserrat', sans-serif" }}>
         <Main />
         <NextScript />
       </body>
