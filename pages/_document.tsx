e729273694a9d7b251bdbffae7b1fd9767b3aa2e
skatesiteadm <designer@cemporcentoskate.com>
2026-02-24 19:17:21 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,13 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="bg-white text-black">
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Halyard+Text:ital,wght@0,400;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet" />
+      </Head>
+      <body className="bg-white text-black" style={{ fontFamily: "'Halyard Text', sans-serif" }}>
         <Main />
         <NextScript />
       </body>
