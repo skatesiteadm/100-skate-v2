d0337dc42083750c9f64ed119e926cae63bfd7f4
skatesiteadm <designer@cemporcentoskate.com>
2026-02-24 23:10:19 -0300

    Update videos.tsx

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -58,7 +58,7 @@ export default function VideosPage() {
           </h3>
 
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
-            {gridVideos.slice(0, 15).map((video) => (
+            {gridVideos.slice(0, 16).map((video) => (
               <button
                 key={video.id}
                 onClick={() => {
