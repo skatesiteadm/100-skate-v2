02baf26eed3fe7935ecc629c2b84de198aed8a4d
skatesiteadm <designer@cemporcentoskate.com>
2026-03-02 20:29:11 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import IndexPage from 'components/IndexPage'
 import PreviewIndexPage from 'components/PreviewIndexPage'
 import { readToken } from 'lib/sanity.api'
 import { getAllPosts, getClient, getSettings } from 'lib/sanity.client'
-import { Post, Settings } from 'lib/sanity.queries'
+import { Post, Revista, Settings, revistaQuery } from 'lib/sanity.queries'
 import { GetStaticProps } from 'next'
 import type { SharedPageProps } from 'pages/_app'
 
 interface PageProps extends SharedPageProps {
   posts: Post[]
   settings: Settings
+  revista: Revista | null
 }
 
 interface Query {
@@ -16,15 +17,13 @@ interface Query {
 }
 
 export default function Page(props: PageProps) {
-  const { posts, settings, previewMode } = props
-
+  const { posts, settings, revista, previewMode } = props
   if (previewMode) {
     return <PreviewIndexPage posts={posts} settings={settings} />
   }
-
   return (
     <main>
-      <IndexPage posts={posts} settings={settings} />
+      <IndexPage posts={posts} settings={settings} revista={revista} />
     </main>
   )
 }
@@ -34,19 +33,20 @@ export const getStaticProps: GetStaticProps<PageProps, Query> = async (ctx) => {
   const client = getClient(
     previewMode ? { token: readToken, perspective: previewData } : undefined,
   )
-
-  const [settings, posts = []] = await Promise.all([
+  const [settings, posts = [], revista = null] = await Promise.all([
     getSettings(client),
     getAllPosts(client),
+    client.fetch(revistaQuery),
   ])
-
   return {
     props: {
       posts,
       settings,
+      revista,
       previewMode,
       previewPerspective: typeof previewData === 'string' ? previewData : null,
       token: previewMode ? readToken : '',
     },
   }
 }
+
