f687a5af3f7f1415e5949371a90fd15a0eefa8ed
skatesiteadm <designer@cemporcentoskate.com>
2026-03-02 17:22:07 -0300

    Update BlogLayout.tsx

--- components/BlogLayout.tsx
+++ components/BlogLayout.tsx
@@ -1,5 +1,6 @@
 import AlertBanner from 'components/AlertBanner'
 import Footer from 'components/Footer'
+import SidebarBanner from 'components/SidebarBanner'
 import StickyBanner from 'components/StickyBanner'
 
 export default function BlogLayout({
@@ -14,7 +15,12 @@ export default function BlogLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <AlertBanner preview={preview} loading={loading} />
-      <main className="flex-1 pb-16 md:pb-0">{children}</main>
+      <main className="flex-1 pb-16 md:pb-0">
+        <div className="flex gap-6 max-w-screen-2xl mx-auto px-4">
+          <div className="flex-1 min-w-0">{children}</div>
+          <SidebarBanner />
+        </div>
+      </main>
       <Footer />
       <StickyBanner />
     </div>
