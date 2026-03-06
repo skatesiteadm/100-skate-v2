9a751bd4b305f7f89afa7e80fd29f554be67f82d
skatesiteadm <designer@cemporcentoskate.com>
2026-03-06 00:01:33 -0300

    Update PostPage.tsx

--- components/PostPage.tsx
+++ components/PostPage.tsx
@@ -35,7 +35,7 @@ export default function PostPage(props: PostPageProps) {
       <PostPageHead settings={settings} post={post} />
       <Layout preview={preview} loading={loading}>
         <Container>
-          <BlogHeader title={title} description={[]} level={1} />
+          <BlogHeader title={title} description={[]} level={1} hideNav />
           {preview && !post ? (
             <PostTitle>Loading...</PostTitle>
           ) : (
