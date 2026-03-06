06fa42eef7379742933246bc1b9e76d6f525612c
skatesiteadm <designer@cemporcentoskate.com>
2026-03-05 23:46:54 -0300

    Update SidebarBanner.tsx

--- components/SidebarBanner.tsx
+++ components/SidebarBanner.tsx
@@ -2,8 +2,10 @@ import BannerSlot from './BannerSlot'
 
 export default function SidebarBanner() {
   return (
-    <div className="hidden xl:flex flex-col gap-4 w-40 shrink-0 sticky top-4">
-      <BannerSlot posicao="sidebar" />
+    <div className="hidden xl:flex flex-col gap-4 w-40 shrink-0">
+      <div className="sticky top-4">
+        <BannerSlot posicao="sidebar" />
+      </div>
     </div>
   )
 }
