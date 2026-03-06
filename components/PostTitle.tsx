962dbc0ef836948181397f5b3d4aede059c381ad
skatesiteadm <designer@cemporcentoskate.com>
2026-03-05 23:40:53 -0300

    Update PostTitle.tsx

--- components/PostTitle.tsx
+++ components/PostTitle.tsx
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="mb-12 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl text-balance">
+    <h1 className="mb-6 text-3xl md:text-4xl lg:text-5xl font-black uppercase leading-tight tracking-tight">
       {children}
     </h1>
   )
